292abf4b481c770649ac59696d645811d0c1cea8
Max Rohowsky, Ph.D. <68344959+MaxRohowsky@users.noreply.github.com>
2025-01-18 19:42:33 +0100

    Update install_extensions.sh

--- src/install_extensions.sh
+++ src/install_extensions.sh
@@ -21,19 +21,19 @@ pip install -r custom_nodes/comfyui_controlnet_aux/requirements.txt
 #IPADAPTER
 git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus custom_nodes/ComfyUI_IPAdapter_plus
 
-# reactor
-git clone https://github.com/Gourieff/comfyui-reactor-node custom_nodes/comfyui-reactor-node
-python custom_nodes/comfyui-reactor-node/install.py
-wget -P ~/models/facerestore_models https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
-wget -P ~/models/facerestore_models https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
+# reactor - has been disabled
+# git clone https://github.com/Gourieff/comfyui-reactor-node custom_nodes/comfyui-reactor-node
+# python custom_nodes/comfyui-reactor-node/install.py
+# wget -P ~/models/facerestore_models https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
+# wget -P ~/models/facerestore_models https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
 
 # WAS node nodes
 git clone https://github.com/WASasquatch/was-node-suite-comfyui custom_nodes/was-node-suite-comfyui
 pip install -r custom_nodes/was-node-suite-comfyui/requirements.txt
 
 # WAS extra nodes
-git clone https://github.com/WASasquatch/WAS_Extras custom_nodes/WAS_Extras
-python custom_nodes/WAS_Extras/install.py
+# git clone https://github.com/WASasquatch/WAS_Extras custom_nodes/WAS_Extras
+# python custom_nodes/WAS_Extras/install.py
 
 #sdxl_prompt_styler
 git clone https://github.com/twri/sdxl_prompt_styler custom_nodes/sdxl_prompt_styler
